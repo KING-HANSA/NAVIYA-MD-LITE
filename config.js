be1f43a5eba21e7a2f7e59ab685c705c3ceaa3c3
KING-HANSA <hansadewmina2008@gmail.com>
2025-02-01 15:12:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "qQgmiBAK#QSIAj4Ke6Ztn9-WdX7W1Rlm12_2sP5jR0mB1EObSpWs",
+MONGODB: process.env.MONGODB || "mongodb+srv://Xcelsama:Xcel@xcelsama.qpklf.mongodb.net/?retryWrites=true&w=majority&appName=Xcelsama",
+};
